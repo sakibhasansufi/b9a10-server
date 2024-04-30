c77ac34b131d5d23e04c01c2c9652d90bea1e065
sakibhasansufi <sakibhasansufi@gmail.com>
2024-04-30 20:52:37 +0600

    delete operation added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -41,6 +41,15 @@ async function run() {
             console.log(newArt);
             const result =await artCollection.insertOne(newArt);
             res.send(result)
+        })
+
+
+
+        app.delete('/art/:id',async(req,res)=>{
+            const id =req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await artCollection.deleteOne(query);
+            res.send(result);
         })
 
 
